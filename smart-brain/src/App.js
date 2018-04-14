48609ede265afa0980c2838777cc2d54311006c5
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-04-14 07:15:42 -0400

    Implement Face recognition API

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -5,7 +5,15 @@ import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+// initialize with your api key. This will also work in your browser via http://browserify.org/
+const app = new Clarifai.App({
+    apiKey: 'e6b2a014ca7a42039725e46d4d1adbe9'
+});
+
 
 const particlesOpt = {
     particles: {
@@ -28,12 +36,40 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            input: ''
+            input: '',
+            imageUrl: '',
+            box: {}
         }
-    }
+    };
 
+    calculateFaceLocation = (data) => {
+       const detectedFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+       const image = document.getElementById('input_image');
+       const width = Number(image.width);
+       const height = Number(image.height);
+       return {
+           leftCol: detectedFace.left_col * width,
+           rightCol: width -  (detectedFace.right_col * width),
+           topRow: detectedFace.top_row * height,
+           bottomRow: height -  (detectedFace.bottom_row * height)
+       }
+    };
+
+    displaceFaceBox = (box) => {
+        console.log(box);
+        this.setState ({box: box});
+    };
     onInputChange = (event) => {
-        console.log(event.target.value);
+        this.setState({input: event.target.value});
+    };
+
+    onSubmit = () => {
+        this.setState({imageUrl: this.state.input});
+        app.models.predict(
+            Clarifai.FACE_DETECT_MODEL,
+            this.state.input)
+            .then ((response) => this.displaceFaceBox(this.calculateFaceLocation(response)))
+            .catch((err) => console.log(err));
     };
 
     render() {
@@ -43,8 +79,8 @@ class App extends Component {
                 <Navigation/>
                 <Logo/>
                 <Rank/>
-                <ImageLinkForm onInputChange = {this.onInputChange}/>
-                {/*<FaceRecognition/>*/}
+                <ImageLinkForm onInputChange = {this.onInputChange} onSubmit = {this.onSubmit}/>
+                <FaceRecognition box = {this.state.box} imageUrl = {this.state.imageUrl}/>
             </div>
         );
     }
