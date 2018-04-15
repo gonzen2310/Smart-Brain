1cda8cd74779944c54e913ec47fe93ee7e1e6ea4
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-04-15 01:59:10 -0400

    Complete server connection

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -42,8 +42,27 @@ class App extends Component {
             imageUrl: '',
             box: {},
             route: 'signin',
-            isSignedIn: false
+            isSignedIn: false,
+            user: {
+                id: '',
+                name: '',
+                email: '',
+                password: '',
+                entries: 0,
+                joined: ''
+            }
         };
+    };
+
+    loadUser = (data) => {
+        this.setState( {user: {
+            id: data.id,
+            name: data.name,
+            email: data.email,
+            password: data.password,
+            entries: data.entries,
+            joined: data.joined
+        }});
     };
 
     calculateFaceLocation = (data) => {
@@ -69,10 +88,25 @@ class App extends Component {
 
     onSubmit = () => {
         this.setState({imageUrl: this.state.input});
-        app.models.predict(
-            Clarifai.FACE_DETECT_MODEL,
-            this.state.input)
-            .then ((response) => this.displaceFaceBox(this.calculateFaceLocation(response)))
+        app.models
+            .predict(
+                Clarifai.FACE_DETECT_MODEL,
+                this.state.input)
+            .then (response => {
+                if (response) {
+                    fetch('http://localhost:3000/image',{
+                        method: 'put',
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify({
+                            id: this.state.user.id
+                        })
+                    })  .then(response => response.json())
+                        .then(count => {
+                            this.setState(Object.assign(this.state.user, {entries:count}))
+                        });
+                }
+                this.displaceFaceBox(this.calculateFaceLocation(response))
+            })
             .catch((err) => console.log(err));
     };
 
@@ -95,13 +129,13 @@ class App extends Component {
                 <Logo/>
                 { this.state.route === 'home'
                     ? <div>
-                        <Rank/>
+                        <Rank name={this.state.user.name} entries={this.state.user.entries}/>
                         <ImageLinkForm onInputChange = {this.onInputChange} onSubmit = {this.onSubmit}/>
                         <FaceRecognition box = {box} imageUrl = {imageUrl}/>
                     </div>
                     : (route === 'signin'
-                        ? <Signin onRouteChange = {this.onRouteChange}/>
-                        : <Register onRouteChange = {this.onRouteChange}/>
+                        ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+                        : <Register onRouteChange = {this.onRouteChange} loadUser = {this.loadUser}/>
                     )
 
                 }
