39579dc92537995d15618c9cc1ce5171225285c3
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-04-16 03:05:07 -0400

    Code Refactoring

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -34,24 +34,27 @@ const particlesOpt = {
     }
 };
 
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+        id: '',
+        name: '',
+        email: '',
+        password: '',
+        entries: 0,
+        joined: ''
+    }
+};
+
 class App extends Component {
     constructor() {
         super();
-        this.state = {
-            input: '',
-            imageUrl: '',
-            box: {},
-            route: 'signin',
-            isSignedIn: false,
-            user: {
-                id: '',
-                name: '',
-                email: '',
-                password: '',
-                entries: 0,
-                joined: ''
-            }
-        };
+        this.state = initialState;
+
     };
 
     loadUser = (data) => {
@@ -103,7 +106,8 @@ class App extends Component {
                     })  .then(response => response.json())
                         .then(count => {
                             this.setState(Object.assign(this.state.user, {entries:count}))
-                        });
+                        })
+                        .catch(console.log);
                 }
                 this.displaceFaceBox(this.calculateFaceLocation(response))
             })
@@ -112,7 +116,7 @@ class App extends Component {
 
     onRouteChange = (route) => {
         if (route === 'signout') {
-            this.setState({isSignedIn: false});
+            this.setState(initialState);
         }
         else if (route === 'home') {
             this.setState({isSignedIn: true});
